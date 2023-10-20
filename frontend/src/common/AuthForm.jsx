f6302d65c61cf933d2f7adee80f18730d4e325b3
Hemit Patel <patelvhemit@gmail.com>
2023-10-20 19:55:41 -0400

    Revert "Update AuthForm.jsx"

--- frontend/src/common/AuthForm.jsx
+++ frontend/src/common/AuthForm.jsx
@@ -13,30 +13,30 @@ const AuthForm = () => {
       align="center"
       justify="center"
       minHeight="85vh"
-      padding={{ base: 4, md: 8 }}
+      padding={8}
     >
       <Button
         onClick={toggleColorMode}
         position="absolute"
-        top={{ base: 2, md: 4 }}
-        right={{ base: 2, md: 4 }}
+        top={4}   // Adjust the top spacing as needed
+        right={4} // Adjust the right spacing as needed
       >
         {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
       </Button>
-      <Heading mb={4} size={{ base: 'lg', md: 'xl' }}>
+      <Heading mb={4} size="xl">
         Welcome to Everyone Class
       </Heading>
       <Text mb={6} textAlign="center">
-        Log in to access/create high-quality STEM education
+        Log in to access/create high quality STEM education
       </Text>
       <Button
         onClick={loginWithRedirect}
         colorScheme="teal"
-        size={{ base: 'md', md: 'lg' }}
+        size="lg"
         fontWeight="bold"
         _hover={{ opacity: 0.8 }}
         _focus={{ outline: 'none' }}
-        boxShadow="md"
+        boxShadow="md" // Add a subtle box shadow
       >
         Log in
       </Button>
